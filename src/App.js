b1d4bf9318d092937d921ea744be008e70e25a37
Akanksha Yadav <programmingwithyadavakanksha.40@gmail.com>
2022-09-28 10:03:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  const [count,setCount] = React.useState(0);
+
+  function Add () {
+    setCount ( prevCount => prevCount + 1);
+  }
+
+  function Subtract () {
+    setCount ( prevCount => prevCount - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div className="counter">
+      <button className="counter--minus" onClick={Subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={Add}>+</button>
+     </div>
+  )
 }
-
-export default App;
